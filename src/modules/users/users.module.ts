07dfb8e3340482f90216f789bd82c2a6123de809
gustavool1 <go8895806@gmail.com>
2023-08-22 17:20:07 -0300

    feat: adding jwt to import

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from 'src/modules/users/controllers/users.controller';
 import { Users } from './entity/users.entity';
 import { UsersServices } from './services/users.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersServices],
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users]),
+    JwtModule.register({
+      global: true,
+      secret: 'secret',
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
 })
 export class UsersModule {}
