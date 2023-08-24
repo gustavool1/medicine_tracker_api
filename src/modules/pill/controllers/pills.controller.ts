2e6ef0cc57e26b23a3366a101fdb0ae959c90d03
gustavool1 <go8895806@gmail.com>
2023-08-24 14:01:52 -0300

    feat: adding guard to pills controller

--- src/modules/pill/controllers/pills.controller.ts
+++ src/modules/pill/controllers/pills.controller.ts
@@ -1,17 +1,26 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { PillServices } from '../services/pills.services';
-import { PillsByDatePayload } from '../dtos/pills-by-date-payload';
 import { Pill } from '../entity/pill.entity';
+import { AuthGuard } from 'src/guards/auth.guard';
+import jwt_decode from 'jwt-decode';
 
 @Controller('pill')
 export class PillController {
   constructor(private readonly pillServices: PillServices) {}
 
-  @Post('byDay')
-  async getPillsByDay(@Body() data: PillsByDatePayload): Promise<Pill[]> {
-    return await this.pillServices.getPillsByDay(data);
+  @UseGuards(AuthGuard)
+  @Get('byDay/:date')
+  async getPillsByDay(@Param('date') date: Date, @Req() req): Promise<Pill[]> {
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedToken = jwt_decode(token);
+
+    return await this.pillServices.getPillsByDay({
+      userId: decodedToken['id'],
+      date,
+    });
   }
 
+  @UseGuards(AuthGuard)
   @Get('takePill/:id')
   async takePill(@Param('id') pillId: string) {
     await this.pillServices.takePill(pillId);
