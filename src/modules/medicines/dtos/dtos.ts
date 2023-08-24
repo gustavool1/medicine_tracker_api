37823a64d78685c3d0b4f46ea9cf17e54bd6f306
gustavool1 <go8895806@gmail.com>
2023-08-24 14:01:26 -0300

    feat: removing unnecessary dto

--- src/modules/medicines/dtos/dtos.ts
+++ src/modules/medicines/dtos/dtos.ts
@@ -1,5 +1,4 @@
-import { CreateMedicineResponseDto } from './create-medicine-response.dto';
 import { CreateMedicineDto } from './create-medicine.dto';
 import { MedicinesByDatePayload } from './medicines-by-date.dto';
 
-export { CreateMedicineDto, MedicinesByDatePayload, CreateMedicineResponseDto };
+export { CreateMedicineDto, MedicinesByDatePayload };
