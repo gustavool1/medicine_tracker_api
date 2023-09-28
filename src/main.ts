e30d9951ad3bf83c120b56d3b21f33266edf9e8f
gustavool1 <go8895806@gmail.com>
2023-09-28 11:19:35 -0300

    feat: running migrations when starting the app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { dataSource } from './database';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  await dataSource.initialize();
+  await dataSource.runMigrations();
   await app.listen(process.env.PORT);
 }
 bootstrap();
